88c6ba8d4a12d1ed9c95ce4ab332de79f9cbd7bd
Kiyun <145196876+Kiyunq@users.noreply.github.com>
2024-12-23 23:21:10 +0100

    Update helpall.js

--- Commands/Utilitaire/helpall.js
+++ Commands/Utilitaire/helpall.js
@@ -14,7 +14,7 @@ exports.run = async (bot, message, args, config, data) => {
     let camgain = gains.camgain
 
     const page1 = new Discord.EmbedBuilder()
-      .setAuthor({ name: "Page d'aide des commandes", iconURL: "https://media.discordapp.net/attachments/1300616641369079819/1320839796775456840/f95a8292c1dd5a9d93229f116ee2f8f3.jpg?ex=676b0f6b&is=6769bdeb&hm=99befc31488745477861ab0a828be22d8f1ae9f5b5363cdbcaba70ae2f2426d3&=&format=webp" })
+      .setAuthor({ name: "Page d'aide des commandes", iconURL: "https://cdn.discordapp.com/attachments/1310033382725521488/1320877815725428736/442580f2868f66e09e6c36bbb5e919f2.jpg?ex=676b32d3&is=6769e153&hm=4f98383b61c1ab387eea534fd2fcc584d5453ba8a1c7f2810c8a8f699ebb80d1&" })
       .setColor(data.color)
       .setDescription(`Prefix du serveur: \`${data.prefix}\`
 Utilisez \`${data.prefix}help [commande]\` pour obtenir des informations sur une commande
@@ -49,8 +49,8 @@ owner, unowner, setprofil, leave
     
      [\`Support du bot\`](https://discord.gg/wazaaa)  |
      \n> Vous gagnez actuellement \`${voicegain} coins\` toutes les 15 minutes lorsque vous êtes en vocal, \`${streamgain} coins\` lorsque vous êtes en stream  et \`${camgain} coins\` lorsque vous activez votre caméra !`)
-      .setFooter({ text: config.footerText, iconURL: "https://media.discordapp.net/attachments/1300616641369079819/1320839796775456840/f95a8292c1dd5a9d93229f116ee2f8f3.jpg?ex=676b0f6b&is=6769bdeb&hm=99befc31488745477861ab0a828be22d8f1ae9f5b5363cdbcaba70ae2f2426d3&=&format=webp" })
-      .setImage("https://media.discordapp.net/attachments/1249042420163674153/1250167077378195526/10056.gif?ex=6669f452&is=6668a2d2&hm=435b6f81e5461dc8259ed9a78e8e2245f07fdb48540ad861ab2ef705b8a15cf1&=&width=764&height=35")
+      .setFooter({ text: config.footerText, iconURL: "https://media.discordapp.net/attachments/1310033382725521488/1320878093832949871/f95a8292c1dd5a9d93229f116ee2f8f3.jpg?ex=676b3316&is=6769e196&hm=64ee69d973e68d9aad99468ee1cfc9148db308438729ca09596b3c62775f6da5&=&format=webp" })
+      .setImage("https://cdn.discordapp.com/attachments/1310033382725521488/1320877815725428736/442580f2868f66e09e6c36bbb5e919f2.jpg?ex=676b32d3&is=6769e153&hm=4f98383b61c1ab387eea534fd2fcc584d5453ba8a1c7f2810c8a8f699ebb80d1&")
       .setTimestamp()
 
     return message.reply({ embeds: [page1] });

df58a5f54b83bd04a98247d0b3277455e6ddf4b2
Kiyun <145196876+Kiyunq@users.noreply.github.com>
2024-12-23 23:23:30 +0100

    Update badges.js

--- Commands/Utilitaire/badges.js
+++ Commands/Utilitaire/badges.js
@@ -13,7 +13,7 @@ exports.run = async (bot, message, args, config, data) => {
     .setTitle(`:mortar_board: Voici vos badges`)
     .setDescription(`Vous n'avez pas de badges !`)
     .setFooter({ text: config.footerText + " | 1/1"})
-    .setThumbnail(`https://media.discordapp.net/attachments/1249042420163674153/1250165092931338250/badges.png?ex=6669f279&is=6668a0f9&hm=198b9a5c5b2fb37ef0f5bd7ef1b7e6eef2ce211c68fad0819c4918794c26c912&=&format=webp&quality=lossless&width=404&height=404`)
+    .setThumbnail(`https://cdn.discordapp.com/attachments/1310033382725521488/1320877815725428736/442580f2868f66e09e6c36bbb5e919f2.jpg?ex=676b32d3&is=6769e153&hm=4f98383b61c1ab387eea534fd2fcc584d5453ba8a1c7f2810c8a8f699ebb80d1&`)
 
     message.reply({ embeds: [embed]})
 }
